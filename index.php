2c97f35c871d41c829cd6099085d55b0f1a66708
RagtimeRebel <pcjohnson2@dons.usfca.edu>
2016-02-27 11:57:11 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$who = "World";
+
+?>
+<html>
+    <head>
+        <title>Hello, World</title>
+    </head>
+    <body>
+        <h1><?php echo("Hello, $who!"); ?></h1>
+    </body>
+</html>
